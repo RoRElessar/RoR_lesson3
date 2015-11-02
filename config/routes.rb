ef76b9b016468d4c7777ae57a279d736a70269a0
RoRElessar <palantiri@ukr.net>
2015-11-02 23:11:50 +0200

    Finished with authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'posts#index'
-
+   root 'sessions#new'
+   match '/signup', to: 'users#new',         via: 'get'
+   match '/signin', to: 'sessions#new',      via: 'get'
+   match '/signout', to: 'sessions#destroy', via: 'delete'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
