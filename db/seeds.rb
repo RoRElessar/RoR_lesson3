f48b7816e9856545bc1773d78ecaa27c52260c83
RoRElessar <palantiri@ukr.net>
2015-11-06 21:07:58 +0200

    Edited seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@ end
 
 user = User.find_by_email('admin@test.com')
 unless user
-  user = User.new(full_name: 'Administrator', email: 'admin@test.com', password: 'password123', admin: true)
+  user = User.new(name: 'Administrator', email: 'admin@test.com', password: 'password123', admin: true)
   user.save!
 end
