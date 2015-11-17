06fb08fd203b7ad76eb747feb1147475245275ab
RoRElessar <palantiri@ukr.net>
2015-11-17 20:13:38 +0200

    finished with votinng system

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  resources :posts
+  resources :posts do
+    member { post :vote }
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
